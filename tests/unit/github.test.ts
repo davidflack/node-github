cbba7c34c7e1110ccdbbae30fbde0275ad402f15
David Flack <david.flack@virginpulse.com>
2021-12-24 12:55:37 -0800

    Add unit test for requestPRInfo

--- tests/unit/github.test.ts
+++ tests/unit/github.test.ts
@@ -1,5 +1,10 @@
-import { validateRequest } from "../../apiRoutes/github/helpers";
 import { Request } from "express";
+import { axiosInstance as axios } from "../../config";
+import {
+  validateRequest,
+  requestPRInfo,
+  GithubPullRequestModel,
+} from "../../apiRoutes/github/helpers";
 
 describe("validateRequest", () => {
   it("rejects promise on bad params", () => {
@@ -29,5 +34,22 @@ describe("validateRequest", () => {
         `/${mockGoodRequest.query.repoOwner}/${mockGoodRequest.query.repoName}/pulls`
       );
     });
+  });
+});
+
+describe("requestPRInfo", () => {
+  it("should return PR's", async () => {
+    const data: GithubPullRequestModel[] = [
+      { id: 1, number: 1, title: "Mock Title", user: { login: "mock author" } },
+    ];
+    const response = { data };
+    const mockGet = jest.spyOn(axios, "get");
+    mockGet.mockResolvedValue(response);
+
+    const mockUrl = "https://bogusHost:2";
+    const res = await requestPRInfo(mockUrl);
+    expect(res).toEqual(response);
+    expect(mockGet).toHaveBeenCalledWith(mockUrl);
+    expect(mockGet).toHaveBeenCalledTimes(1);
   });
 });
